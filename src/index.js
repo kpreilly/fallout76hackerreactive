f1abb750e7a4f17a0f17256f68e689cf0344e042
Kyle Reilly <reilly.kyle101@gmail.com>
2022-12-16 15:08:02 -0800

    Updated page styling

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/custom.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App className="bg-dark"/>, document.getElementById('root'));
